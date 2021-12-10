1660a1653cede094cededc71e7935ac40630a507
RAV1OL33 <53752463+RAV1OL33@users.noreply.github.com>
2021-12-10 15:21:48 +0300

    Update script.js
    add some more sites

--- script.js
+++ script.js
@@ -8,6 +8,8 @@ etf_site_array = [
   { site_name: "etfdb.com", url: "https://etfdb.com/etf/" },
   { site_name: "etfrc.com", url: "https://www.etfrc.com/" },
   { site_name: "wsj.com", url: "https://www.wsj.com/market-data/quotes/" },
+  { site_name: "money.cnn.com", url: "https://money.cnn.com/quote/forecast/forecast.html?symb=" },
+  { site_name: "finance.yahoo.com", url: "https://finance.yahoo.com/quote/JPM/analysis?p=" },
 ];
 stocks_site_array = [
   { site_name: "finviz.com", url: "https://finviz.com/quote.ashx?t=" },
@@ -16,6 +18,8 @@ stocks_site_array = [
   { site_name: "tipranks.com", url: "https://www.tipranks.com/stocks/" },
   { site_name: "openinsider.com", url: "http://www.openinsider.com/search?q=" },
   { site_name: "wsj.com", url: "https://www.wsj.com/market-data/quotes/" },
+  { site_name: "money.cnn.com", url: "https://money.cnn.com/quote/forecast/forecast.html?symb=" },
+  { site_name: "finance.yahoo.com", url: "https://finance.yahoo.com/quote/JPM/analysis?p=" },
 ];
 function updateSearchResults(input_text) {
   //console.log(input_text.value)
